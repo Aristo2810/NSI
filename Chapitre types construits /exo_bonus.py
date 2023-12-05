5a54cd722da45a19600e788540d68ded5f7a1b28
Aristo <126325785+enisbeziau@users.noreply.github.com>
2023-12-05 16:05:05 +0100

    Update exo_bonus.py

--- Chapitre types construits /exo_bonus.py
+++ Chapitre types construits /exo_bonus.py
@@ -1,17 +1,19 @@
-"""On considère le jeu suivant : on a un plateau de n cases numérotées de 0 à n−1. Un enfant écrit sur chaque case un
-numéro de case (il peut écrire le même numéro sur plusieurs cases différentes ; il peut écrire le numéro d’une case
-dans la case elle-même ou pas.) On place un pion sur la case numéro 0 et on procède comme suit : dans la case sur
-laquelle est posée le pion, il y a un numéro. On lit le numéro, appelons le N, puis on déplace le pion jusqu’à la
-case N. On recommence en déplacant le pion jusqu’à arriver sur la case numéro 1. Remarque : il est possible qu’on
-déplace indéfiniment le pion sans jamais arriver sur la case 1 Exemple : imaginons que le plateau soit : 3 2 8 4 2 3
-6 3 1. On démarre sur la case 0 ; il est écrit 3 donc on va sur la case 3, qui nous envoie sur la case 4,
-qui nous envoie sur la case 2, qui nous envoie sur la case 8 qui nous envoie sur la case 1. En 5 coups on est donc
-arrivé à la case 1.
+"""
+Auteur : Enis Béziau
 """
 
 
 def jeu(plateau):
     """
+    "On considère le jeu suivant : on a un plateau de n cases numérotées de 0 à n−1. Un enfant écrit sur chaque case un
+    numéro de case (il peut écrire le même numéro sur plusieurs cases différentes ; il peut écrire le numéro d’une case
+    dans la case elle-même ou pas.) On place un pion sur la case numéro 0 et on procède comme suit : dans la case sur
+    laquelle est posée le pion, il y a un numéro. On lit le numéro, appelons le N, puis on déplace le pion jusqu’à la
+    case N. On recommence en déplacant le pion jusqu’à arriver sur la case numéro 1. Remarque : il est possible qu’on
+    déplace indéfiniment le pion sans jamais arriver sur la case 1 Exemple : imaginons que le plateau soit : 3 2 8 4 2 3
+    6 3 1. On démarre sur la case 0 ; il est écrit 3 donc on va sur la case 3, qui nous envoie sur la case 4,
+    qui nous envoie sur la case 2, qui nous envoie sur la case 8 qui nous envoie sur la case 1. En 5 coups on est donc
+    arrivé à la case 1.
     :param list[int] plateau: Le plateau du jeu
     :return: −1 si le jeu ne se termine pas ou le nombre de déplacements pour atteindre la case 1 s’il se termine
     :rtype: int
@@ -32,3 +34,26 @@ assert jeu([0, 4, 10, 2, 3]) == -1
 assert jeu([1, 3, 4, 2, 3]) == 1
 assert jeu([2, 1, 3, 0]) == -1
 assert jeu([2, 1, 3, 1]) == 3
+
+
+def decryptage(num):
+    """
+    0 <--> 5
+    1 <--> 9
+    2 <--> 8
+    3 <--> 7
+    4 <--> 6
+    :param str num: le numéro de téléphone à décrypter / crypter
+    :return: Le num de tel décrypté / crypté selonla symétrie des nums sur un clavier
+    :rtype: str
+    """
+    nouveau_num = ''
+    table = [5, 8, 6, 4, 2, -5, -2, -4, -6, -8]
+    for cara in num:
+        nouveau_num += str(int(cara) + table[int(cara)])
+    return nouveau_num
+
+
+assert decryptage('0123456789') == '5987604321'
+assert decryptage('') == ''
+assert decryptage('0123481055') == '5987629500'
